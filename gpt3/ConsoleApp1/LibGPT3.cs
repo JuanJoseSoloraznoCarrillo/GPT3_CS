0ab76854605ef8491d6f26c7c9c2d8f6497f9ae1
JuanJoseSolo <juanjose.solorzano.c@gmail.com>
2023-06-06 00:07:40 -0600

    Added a user choices

---
+++ gpt3/ConsoleApp1/LibGPT3.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+
+namespace gpt3
+{
+    public class LibGPT3
+    {
+        // Define the API endpoint URL and your API key
+        string apiUrl = string.Empty;
+        string apiKey = string.Empty;
+        string model = string.Empty; 
+        HttpClient? client;
+        public LibGPT3() 
+        {
+            apiUrl = "https://api.openai.com/v1/chat/completions";
+            apiKey = "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";
+            model  = "gpt-3.5-turbo"; // Specify the desired model version
+            // Create an instance of HttpClient
+            client = new HttpClient();
+        }
+
+        public async Task<string> SendChatRequest(string message = "hello")
+        {
+            // Prepare the request data
+            var requestData = new
+            {
+                messages = new[] { new { role = "system", content = "Hello, can you help me?." }, new { role = "user", content = message } },
+                model = model
+            };
+            // Serialize the request data
+            var content = new StringContent(JsonConvert.SerializeObject(requestData), new UTF8Encoding(false), "application/json");
+
+            // Add the API key to the request headers
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
+            // Send the request and get the response
+            var response = await client.PostAsync(apiUrl, content);
+
+            // Read the response content
+            var responseContent = await response.Content.ReadAsStringAsync();
+            // Parse the response content as a JSON object
+            JObject responseObject = JObject.Parse(responseContent);
+            var responseOne = responseObject.Last.ToString();
+            var finalResponse = responseOne.Split("content")[1].Split('}')[0];
+            
+            return finalResponse.Split(":")[1];
+        }
+
+
+    }
+}
--- gpt3/ConsoleApp1/Program.cs
+++ gpt3/ConsoleApp1/Program.cs
@@ -6,52 +6,50 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
+using gpt3;
 
 class Program
 {
     static async Task Main(string[] args)
     {
-        // Create an instance of HttpClient
-        HttpClient client = new HttpClient();
+        // Create the GPT3 object from LibGPT3 module.
+        LibGPT3 gpt3 = new LibGPT3();
 
-        // Define the API endpoint URL and your API key
-        string apiUrl = "https://api.openai.com/v1/chat/completions";
-        string apiKey = "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";
-        string model = "gpt-3.5-turbo"; // Specify the desired model version
+        Console.WriteLine("What would you like to do?\n1-Translate.\n2-Grammar checker.\n3-Python expert.\n4-General propose.");
+        var userInput = Console.ReadLine();
+        string mainTask = string.Empty;
+        string label = string.Empty;
+        if (userInput.Contains("1"))
+        {
+            mainTask = "Translate ";
+            label = "[Translation] ";
+        }
+        else if (userInput.Contains("2"))
+        {
+            mainTask = "Check the grammar of: ";
+            label = "[Grammar] ";
+        }
+        else if (userInput.Contains("3"))
+        {
+            mainTask = "As python expert: ";
+            label = "[Phyton] ";
+        }
+        else
+        {
+            mainTask = "";
+            label = "[General] ";
+        }
+
+        Console.Write($"{label}Send mesage --> ");
+        var userMsg = Console.ReadLine();
+        string userMessage = string.Format("{0}'{1}'", mainTask, userMsg);
+        Console.WriteLine($"You are sending ----> {userMessage}");
         // Call the SendChatRequest method
-        string userMessage = string.Join(" ", args);
-        string response = await SendChatRequest(client, apiUrl, apiKey, model, userMessage);
+        string response = await gpt3.SendChatRequest(userMessage);
 
         // Print the API response
         Console.WriteLine(response);
 
         Console.ReadKey();
-    }
-
-    static async Task<string> SendChatRequest(HttpClient client, string apiUrl, string apiKey, string model, string message = "hello")
-    {
-        // Prepare the request data
-        var requestData = new
-        {
-            messages = new[] { new { role = "system", content = "You are a helpful assistant." }, new { role = "user", content = message } },
-            model = model
-        };
-        // Serialize the request data
-        var content = new StringContent(JsonConvert.SerializeObject(requestData), new UTF8Encoding(false), "application/json");
-
-        // Add the API key to the request headers
-        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
-
-        // Send the request and get the response
-        var response = await client.PostAsync(apiUrl, content);
-
-        // Read the response content
-        var responseContent = await response.Content.ReadAsStringAsync();
-        // Parse the response content as a JSON object
-        JObject responseObject = JObject.Parse(responseContent);
-        var responseOne = responseObject.Last.ToString();
-        var finalResponse = responseOne.Split("content")[1].Split('}')[0];
-        
-        return finalResponse.Split(":")[1];
     }
 }
